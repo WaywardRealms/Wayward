cf4d2cc2b082e3de76d30c55013af9be755fb8c4
Lucariatias <lucariatias@gmail.com>
2014-03-21 19:49:43 +0000

    Add methods to get material tool types

--- WaywardLib/src/main/java/net/wayward_realms/waywardlib/professions/ToolType.java
+++ WaywardLib/src/main/java/net/wayward_realms/waywardlib/professions/ToolType.java
@@ -12,12 +12,32 @@ public enum ToolType {
     AXE(WOOD_AXE, STONE_AXE, IRON_AXE, GOLD_AXE, DIAMOND_AXE),
     SPADE(WOOD_SPADE, STONE_SPADE, IRON_SPADE, GOLD_SPADE, DIAMOND_SPADE);
 
+    /**
+     * Gets the tooltype of a material
+     *
+     * @param material the material
+     * @return the tooltype, or none if the material is not a tool
+     */
+    public static ToolType getToolType(Material material) {
+        if (material.toString().endsWith("_PICKAXE")) return PICKAXE;
+        if (material.toString().endsWith("_SWORD")) return SWORD;
+        if (material.toString().endsWith("_HOE")) return HOE;
+        if (material.toString().endsWith("_AXE")) return AXE;
+        if (material.toString().endsWith("_SPADE")) return SPADE;
+        return null;
+    }
+
     private final Material[] materials;
 
     private ToolType(Material... materials) {
         this.materials = materials;
     }
 
+    /**
+     * Gets the materials of the tooltype
+     *
+     * @return the materials of the tool's type
+     */
     public Material[] getMaterials() {
         return materials;
     }
