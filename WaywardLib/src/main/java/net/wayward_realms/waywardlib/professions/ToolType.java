7c8c0227666023e7e0121eae53f71072e0e4daf8
Lucariatias <lucariatias@gmail.com>
2014-03-23 23:07:03 +0000

    Add armour to ToolType

--- WaywardLib/src/main/java/net/wayward_realms/waywardlib/professions/ToolType.java
+++ WaywardLib/src/main/java/net/wayward_realms/waywardlib/professions/ToolType.java
@@ -10,7 +10,12 @@ public enum ToolType {
     SWORD(WOOD_SWORD, STONE_SWORD, IRON_SWORD, GOLD_SWORD, DIAMOND_SWORD),
     HOE(WOOD_HOE, STONE_HOE, IRON_HOE, GOLD_HOE, DIAMOND_HOE),
     AXE(WOOD_AXE, STONE_AXE, IRON_AXE, GOLD_AXE, DIAMOND_AXE),
-    SPADE(WOOD_SPADE, STONE_SPADE, IRON_SPADE, GOLD_SPADE, DIAMOND_SPADE);
+    SPADE(WOOD_SPADE, STONE_SPADE, IRON_SPADE, GOLD_SPADE, DIAMOND_SPADE),
+
+    HELMET(LEATHER_HELMET, CHAINMAIL_HELMET, GOLD_HELMET, IRON_HELMET, DIAMOND_HELMET),
+    CHESTPLATE(LEATHER_CHESTPLATE, CHAINMAIL_CHESTPLATE, GOLD_CHESTPLATE, IRON_CHESTPLATE, DIAMOND_CHESTPLATE),
+    LEGGINGS(LEATHER_LEGGINGS, CHAINMAIL_LEGGINGS, GOLD_LEGGINGS, IRON_LEGGINGS, DIAMOND_LEGGINGS),
+    BOOTS(LEATHER_BOOTS, CHAINMAIL_BOOTS, GOLD_BOOTS, IRON_BOOTS, DIAMOND_BOOTS);
 
     /**
      * Gets the tooltype of a material
@@ -24,6 +29,10 @@ public enum ToolType {
         if (material.toString().endsWith("_HOE")) return HOE;
         if (material.toString().endsWith("_AXE")) return AXE;
         if (material.toString().endsWith("_SPADE")) return SPADE;
+        if (material.toString().endsWith("_HELMET")) return HELMET;
+        if (material.toString().endsWith("_CHESTPLATE")) return CHESTPLATE;
+        if (material.toString().endsWith("_LEGGINGS")) return LEGGINGS;
+        if (material.toString().endsWith("_BOOTS")) return BOOTS;
         return null;
     }
 
