b015de1a92d5576951415bbf524bc07ce219e9c1
Lucariatias <lucariatias@gmail.com>
2014-03-25 17:58:36 +0000

    Remove wounded messages

--- WaywardCharacters/src/main/java/net/wayward_realms/waywardcharacters/EntityDamageListener.java
+++ WaywardCharacters/src/main/java/net/wayward_realms/waywardcharacters/EntityDamageListener.java
@@ -1,6 +1,5 @@
 package net.wayward_realms.waywardcharacters;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,20 +20,6 @@ public class EntityDamageListener implements Listener {
         if ((event.getEntity() instanceof Player)) {
             Player damaged = (Player) event.getEntity();
             plugin.getActiveCharacter(damaged).setHealth(Math.max(damaged.getHealth() - event.getDamage(), 0));
-            if ((((float) damaged.getHealth() / (float) damaged.getMaxHealth()) * 100.0D > 25.0D) && (((float) damaged.getHealth() / (float) damaged.getMaxHealth()) * 100.0D <= 50.0D)) {
-                for (Player player : event.getEntity().getWorld().getPlayers()) {
-                    if (event.getEntity().getLocation().distance(player.getLocation()) <= MAX_DISTANCE) {
-                        player.sendMessage(plugin.getPrefix() + ChatColor.RED + ((Player) event.getEntity()).getDisplayName() + " appears wounded!");
-                    }
-                }
-            }
-            if (((float) damaged.getHealth() / (float) damaged.getMaxHealth()) * 100.0D <= 25.0D) {
-                for (Player player : event.getEntity().getWorld().getPlayers()) {
-                    if (event.getEntity().getLocation().distance(player.getLocation()) <= MAX_DISTANCE) {
-                        player.sendMessage(plugin.getPrefix() + ChatColor.RED + ((Player) event.getEntity()).getDisplayName() + " appears gravely wounded!");
-                    }
-                }
-            }
         }
     }
 
