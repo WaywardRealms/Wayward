eb6164ce73c6313b98f26ce847be42118434b1b0
Lucariatias <lucariatias@gmail.com>
2014-05-08 21:41:25 +0100

    Validation on death

--- WaywardUnconsciousness/src/main/java/net/wayward_realms/waywardunconsciousness/PlayerDeathListener.java
+++ WaywardUnconsciousness/src/main/java/net/wayward_realms/waywardunconsciousness/PlayerDeathListener.java
@@ -1,7 +1,6 @@
 package net.wayward_realms.waywardunconsciousness;
 
 import net.wayward_realms.waywardlib.character.CharacterPlugin;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,42 +22,59 @@ public class PlayerDeathListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         event.setDeathMessage("");
-        switch (event.getEntity().getLastDamageCause().getCause()) {
-            case FIRE:
-            case FIRE_TICK:
-            case LAVA:
-            case DROWNING:
-                RegisteredServiceProvider<CharacterPlugin> characterPluginProvider = plugin.getServer().getServicesManager().getRegistration(CharacterPlugin.class);
-                if (characterPluginProvider != null) {
-                    CharacterPlugin characterPlugin = characterPluginProvider.getProvider();
-                    characterPlugin.getActiveCharacter(event.getEntity()).setDead(true);
-                    characterPlugin.setActiveCharacter(event.getEntity(), characterPlugin.createNewCharacter(event.getEntity()));
-                    event.getDrops().clear();
-                    event.getEntity().sendMessage(plugin.getPrefix() + ChatColor.RED + "Your death was determined to be a fatality, and a new character has been created for you.");
-                }
-                break;
-            case FALL:
-                if (event.getEntity().getFallDistance() >= 64) {
-                    characterPluginProvider = plugin.getServer().getServicesManager().getRegistration(CharacterPlugin.class);
-                    if (characterPluginProvider != null) {
-                        CharacterPlugin characterPlugin = characterPluginProvider.getProvider();
-                        characterPlugin.getActiveCharacter(event.getEntity()).setDead(true);
-                        characterPlugin.setActiveCharacter(event.getEntity(), characterPlugin.createNewCharacter(event.getEntity()));
+        if (event.getEntity().getLastDamageCause() != null) {
+            if (event.getEntity().getLastDamageCause().getCause() != null) {
+                switch (event.getEntity().getLastDamageCause().getCause()) {
+                    case FIRE:
+                    case FIRE_TICK:
+                    case LAVA:
+                    case DROWNING:
+                        RegisteredServiceProvider<CharacterPlugin> characterPluginProvider = plugin.getServer().getServicesManager().getRegistration(CharacterPlugin.class);
+                        if (characterPluginProvider != null) {
+                            CharacterPlugin characterPlugin = characterPluginProvider.getProvider();
+                            characterPlugin.getActiveCharacter(event.getEntity()).setDead(true);
+                            characterPlugin.setActiveCharacter(event.getEntity(), characterPlugin.createNewCharacter(event.getEntity()));
+                            event.getDrops().clear();
+                            event.getEntity().sendMessage(plugin.getPrefix() + ChatColor.RED + "Your death was determined to be a fatality, and a new character has been created for you.");
+                        }
+                        break;
+                    case FALL:
+                        if (event.getEntity().getFallDistance() >= 64) {
+                            characterPluginProvider = plugin.getServer().getServicesManager().getRegistration(CharacterPlugin.class);
+                            if (characterPluginProvider != null) {
+                                CharacterPlugin characterPlugin = characterPluginProvider.getProvider();
+                                characterPlugin.getActiveCharacter(event.getEntity()).setDead(true);
+                                characterPlugin.setActiveCharacter(event.getEntity(), characterPlugin.createNewCharacter(event.getEntity()));
+                                event.getDrops().clear();
+                                event.getEntity().sendMessage(plugin.getPrefix() + ChatColor.RED + "Your death was determined to be a fatality, and a new character has been created for you.");
+                            }
+                        }
+                        break;
+                    default:
+                        plugin.setUnconscious(event.getEntity(), true);
+                        plugin.setDeathLocation(event.getEntity(), event.getEntity().getLocation());
+                        plugin.setDeathTime(event.getEntity());
+                        List<ItemStack> drops = new ArrayList<>(event.getDrops());
+                        plugin.setDeathInventory(event.getEntity(), drops);
                         event.getDrops().clear();
-                        event.getEntity().sendMessage(plugin.getPrefix() + ChatColor.RED + "Your death was determined to be a fatality, and a new character has been created for you.");
-                    }
+                        break;
                 }
-                break;
-            default:
+            } else {
                 plugin.setUnconscious(event.getEntity(), true);
                 plugin.setDeathLocation(event.getEntity(), event.getEntity().getLocation());
                 plugin.setDeathTime(event.getEntity());
                 List<ItemStack> drops = new ArrayList<>(event.getDrops());
                 plugin.setDeathInventory(event.getEntity(), drops);
                 event.getDrops().clear();
-                break;
+            }
+        } else {
+            plugin.setUnconscious(event.getEntity(), true);
+            plugin.setDeathLocation(event.getEntity(), event.getEntity().getLocation());
+            plugin.setDeathTime(event.getEntity());
+            List<ItemStack> drops = new ArrayList<>(event.getDrops());
+            plugin.setDeathInventory(event.getEntity(), drops);
+            event.getDrops().clear();
         }
-
     }
 
 }
