d20a54684d928961a65fcf530002b14165d36f5b
Lucariatias <lucariatias@gmail.com>
2014-05-29 13:28:04 +0100

    Fix YAML file filter

--- WaywardLib/src/main/java/net/wayward_realms/waywardlib/util/file/filter/YamlFileFilter.java
+++ WaywardLib/src/main/java/net/wayward_realms/waywardlib/util/file/filter/YamlFileFilter.java
@@ -6,7 +6,7 @@ package net.wayward_realms.waywardlib.util.file.filter;
 public class YamlFileFilter extends FileExtensionFilter {
 
     public YamlFileFilter() {
-        super(".yml");
+        super("yml");
     }
 
 }
