1507e1518d30cd4a3c696c8323ba98011f1d0170
Lucariatias <lucariatias@gmail.com>
2014-05-31 02:04:58 +0100

    Add some new recipes

--- WaywardItems/src/main/java/net/wayward_realms/waywarditems/recipe/RecipeManager.java
+++ WaywardItems/src/main/java/net/wayward_realms/waywarditems/recipe/RecipeManager.java
@@ -15,11 +15,39 @@ public class RecipeManager {
 
     public void setupRecipes() {
         setupSaddleRecipe();
+        setupLeashRecipe();
+        setupIronHorseArmourRecipe();
+        setupGoldHorseArmourRecipe();
+        setupDiamondHorseArmourRecipe();
     }
 
     private void setupSaddleRecipe() {
         ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.SADDLE));
         recipe.shape("lll", "sls", " i ").setIngredient('l', Material.LEATHER).setIngredient('s', Material.STRING).setIngredient('i', Material.IRON_INGOT);
+        plugin.getServer().addRecipe(recipe);
+    }
+
+    private void setupLeashRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.LEASH));
+        recipe.shape(" ss", " ss", "s  ").setIngredient('s', Material.STRING);
+        plugin.getServer().addRecipe(recipe);
+    }
+
+    private void setupIronHorseArmourRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.IRON_BARDING));
+        recipe.shape("  i", "lIl", "i i").setIngredient('i', Material.IRON_INGOT).setIngredient('l', Material.LEATHER).setIngredient('I', Material.IRON_BLOCK);
+        plugin.getServer().addRecipe(recipe);
+    }
+
+    private void setupGoldHorseArmourRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.GOLD_BARDING));
+        recipe.shape("  g", "lGl", "g g").setIngredient('g', Material.GOLD_INGOT).setIngredient('l', Material.LEATHER).setIngredient('G', Material.GOLD_BLOCK);
+        plugin.getServer().addRecipe(recipe);
+    }
+
+    private void setupDiamondHorseArmourRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.DIAMOND_BARDING));
+        recipe.shape("  d", "lDl", "d d").setIngredient('d', Material.DIAMOND).setIngredient('l', Material.LEATHER).setIngredient('D', Material.DIAMOND_BLOCK);
         plugin.getServer().addRecipe(recipe);
     }
 
