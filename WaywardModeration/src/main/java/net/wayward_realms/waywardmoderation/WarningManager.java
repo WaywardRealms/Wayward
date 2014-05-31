c0c824430e77864a888e45fc5ea612b3012156b1
Lucariatias <lucariatias@gmail.com>
2014-05-31 12:12:20 +0100

    Properly implement warning storage

--- WaywardModeration/src/main/java/net/wayward_realms/waywardmoderation/WarningManager.java
+++ WaywardModeration/src/main/java/net/wayward_realms/waywardmoderation/WarningManager.java
@@ -2,26 +2,54 @@ package net.wayward_realms.waywardmoderation;
 
 import net.wayward_realms.waywardlib.moderation.Warning;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 
 public class WarningManager {
 
-    private Map<String, Set<Warning>> warnings = new HashMap<>();
+    private WaywardModeration plugin;
+
+    public WarningManager(WaywardModeration plugin) {
+        this.plugin = plugin;
+    }
 
     public Collection<Warning> getWarnings(OfflinePlayer player) {
-        if (warnings.get(player.getName()) == null) {
-            warnings.put(player.getName(), new HashSet<Warning>());
-        }
-        return warnings.get(player.getName());
+        File warningsDirectory = new File(plugin.getDataFolder(), "warnings");
+        File warningsFile = new File(warningsDirectory, player.getName() + ".yml");
+        YamlConfiguration warningsSave = YamlConfiguration.loadConfiguration(warningsFile);
+        return (List<Warning>) warningsSave.getList("warnings");
     }
 
     public void addWarning(OfflinePlayer player, Warning warning) {
-        getWarnings(player).add(warning);
+        File warningsDirectory = new File(plugin.getDataFolder(), "warnings");
+        File warningsFile = new File(warningsDirectory, player.getName() + ".yml");
+        YamlConfiguration warningsSave = YamlConfiguration.loadConfiguration(warningsFile);
+        List<Warning> warnings = (List<Warning>) warningsSave.getList("warnings");
+        warnings.add(warning);
+        warningsSave.set("warnings", warnings);
+        try {
+            warningsSave.save(warningsFile);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void removeWarning(OfflinePlayer player, Warning warning) {
-        getWarnings(player).remove(warning);
+        File warningsDirectory = new File(plugin.getDataFolder(), "warnings");
+        File warningsFile = new File(warningsDirectory, player.getName() + ".yml");
+        YamlConfiguration warningsSave = YamlConfiguration.loadConfiguration(warningsFile);
+        List<Warning> warnings = (List<Warning>) warningsSave.getList("warnings");
+        warnings.remove(warning);
+        warningsSave.set("warnings", warnings);
+        try {
+            warningsSave.save(warningsFile);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
 }
