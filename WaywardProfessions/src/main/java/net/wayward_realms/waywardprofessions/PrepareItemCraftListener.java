6bf4066fc95adcf4ec2eb694c86fc2d97cb034e0
Lucariatias <lucariatias@gmail.com>
2014-06-04 21:23:05 +0100

    Trim out some useless commented lines

--- WaywardProfessions/src/main/java/net/wayward_realms/waywardprofessions/PrepareItemCraftListener.java
+++ WaywardProfessions/src/main/java/net/wayward_realms/waywardprofessions/PrepareItemCraftListener.java
@@ -34,20 +34,16 @@ public class PrepareItemCraftListener implements Listener {
                     ItemStack itemWithAdjustedDurability = event.getInventory().getResult();
                     itemWithAdjustedDurability.setDurability((short) (event.getInventory().getResult().getType().getMaxDurability() - (0.75D * (double) plugin.getMaxToolDurability(character, type))));
                     event.getInventory().setResult(itemWithAdjustedDurability);
-                    //Random random = new Random();
-                    //plugin.setMaxToolDurability(character, type, Math.min(plugin.getMaxToolDurability(character, type) + (random.nextInt(100) <= 30 ? random.nextInt(3) : 0), event.getInventory().getResult().getType().getMaxDurability()));
                 }
             }
             RegisteredServiceProvider<CharacterPlugin> characterPluginProvider = Bukkit.getServer().getServicesManager().getRegistration(CharacterPlugin.class);
             if (characterPluginProvider != null) {
                 CharacterPlugin characterPlugin = characterPluginProvider.getProvider();
                 net.wayward_realms.waywardlib.character.Character character = characterPlugin.getActiveCharacter((Player) event.getViewers().get(0));
-                //Material material = event.getInventory().getResult().getType();
                 Random random = new Random();
                 int craftEfficiency = plugin.getCraftEfficiency(character, event.getInventory().getResult().getType());
                 int amount = (int) ((double) (random.nextInt(100) <= 30 ? (craftEfficiency > 10 ? random.nextInt(craftEfficiency) : random.nextInt(10)) : 25) * (4D / 100D) * (double) event.getInventory().getResult().getAmount());
                 event.getInventory().getResult().setAmount(amount);
-                //plugin.setCraftEfficiency(character, material, plugin.getCraftEfficiency(character, material) + (random.nextInt(100) <= 30 ? random.nextInt(3) : 0));
             }
         }
     }
