521f11e4c81f8f594a2e144e3510838b01491ee1
Lucariatias <lucariatias@gmail.com>
2014-06-09 23:22:02 +0100

    Add status effect enum, #37

---
+++ WaywardLib/src/main/java/net/wayward_realms/waywardlib/combat/StatusEffect.java
@@ -0,0 +1,15 @@
+package net.wayward_realms.waywardlib.combat;
+
+public enum StatusEffect {
+
+    POISON,
+    PARALYSIS,
+    BURNED,
+    FROZEN,
+    CONFUSED,
+    ASLEEP,
+    BLIND,
+    DOOM,
+    SILENCED
+
+}
