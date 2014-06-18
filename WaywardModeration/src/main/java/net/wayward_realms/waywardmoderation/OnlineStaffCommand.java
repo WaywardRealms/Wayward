657818e5c9ce845c3763512da77d938691331393
Lucariatias <lucariatias@gmail.com>
2014-06-18 16:58:08 +0100

    Add IRC users to online staff, #68

--- WaywardModeration/src/main/java/net/wayward_realms/waywardmoderation/OnlineStaffCommand.java
+++ WaywardModeration/src/main/java/net/wayward_realms/waywardmoderation/OnlineStaffCommand.java
@@ -1,10 +1,13 @@
 package net.wayward_realms.waywardmoderation;
 
+import net.wayward_realms.waywardlib.chat.Channel;
+import net.wayward_realms.waywardlib.chat.ChatPlugin;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class OnlineStaffCommand implements CommandExecutor {
 
@@ -20,6 +23,21 @@ public class OnlineStaffCommand implements CommandExecutor {
         for (Player player : plugin.getServer().getOnlinePlayers()) {
             if (player.hasPermission("wayward.moderation.staff")) {
                 sender.sendMessage(ChatColor.GREEN + player.getName());
+            }
+        }
+
+        RegisteredServiceProvider<ChatPlugin> chatPluginProvider = plugin.getServer().getServicesManager().getRegistration(ChatPlugin.class);
+        if (chatPluginProvider != null) {
+            ChatPlugin chatPlugin = chatPluginProvider.getProvider();
+            sender.sendMessage(ChatColor.GRAY + "----------------");
+            sender.sendMessage(plugin.getPrefix() + ChatColor.GREEN + "Staff in IRC:");
+            for (Channel channel : chatPlugin.getChannels()) {
+                if (channel.getIrcChannel() != null) {
+                    sender.sendMessage(ChatColor.GRAY + channel.getIrcChannel() + ":");
+                    for (String user : chatPlugin.getStaffInIrcChannel(channel.getIrcChannel())) {
+                        sender.sendMessage(ChatColor.GREEN + user);
+                    }
+                }
             }
         }
         return true;
