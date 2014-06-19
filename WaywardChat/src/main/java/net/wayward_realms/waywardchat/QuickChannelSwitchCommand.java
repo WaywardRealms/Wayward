1fc02a701a165b9628dd72e8c2dc732a59ebe6f3
Lucariatias <lucariatias@gmail.com>
2014-06-19 12:18:45 +0100

    Revert "Fixed compatibility issue with QuickChannelSwitchCommand, and generally cleaned things up over there."
    This reverts commit 1a18e7df81960fe71ec47c348c27e9af26f8eed8.

--- WaywardChat/src/main/java/net/wayward_realms/waywardchat/QuickChannelSwitchCommand.java
+++ WaywardChat/src/main/java/net/wayward_realms/waywardchat/QuickChannelSwitchCommand.java
@@ -1,36 +1,38 @@
 package net.wayward_realms.waywardchat;
 
 import net.wayward_realms.waywardlib.chat.Channel;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class QuickChannelSwitchCommand implements CommandExecutor {
+import java.util.List;
+
+public class QuickChannelSwitchCommand extends Command {
 
     private WaywardChat plugin;
+    private Channel channel;
 
-    public QuickChannelSwitchCommand(WaywardChat inplugin){
-        this.plugin = inplugin;
+    public QuickChannelSwitchCommand(String name, String description, List<String> aliases, WaywardChat plugin, Channel channel) {
+        super(name, description, "/<command> [message]", aliases);
+        this.plugin = plugin;
+        this.channel = channel;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender.hasPermission("wayward.chat.ch.talkin." + cmd.getName())) {
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (sender.hasPermission("wayward.chat.ch.talkin." + this.channel.getName())) {
             Channel channel = plugin.getPlayerChannel((Player) sender);
-            plugin.setPlayerChannel((Player) sender, plugin.getChannel(cmd.getName()));
+            plugin.setPlayerChannel((Player) sender, this.channel);
             if (args.length >= 1) {
                 String message = "";
                 for (String arg : args) {
                     message += arg + " ";
                 }
-                sender.sendMessage(plugin.getPrefix() + channel.getColour() + "Now talking in " + channel.getName() + ".");
-                Bukkit.getServer().getPluginManager().callEvent(new AsyncPlayerChatEvent(true, (Player) sender, message, null));
+                plugin.handleChat((Player) sender, message);
+                plugin.setPlayerChannel((Player) sender, channel);
             } else {
-                sender.sendMessage(plugin.getPrefix() + channel.getColour() + "Now talking in " + channel.getName() + ".");
+                sender.sendMessage(plugin.getPrefix() + this.channel.getColour() + "Now talking in " + this.channel.getName() + ".");
             }
         } else {
             sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "You do not have permission!");
