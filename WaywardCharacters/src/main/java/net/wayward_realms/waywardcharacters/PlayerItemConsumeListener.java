f438221ce97631d3d4fd533708cead4c7f68977a
Lucariatias <lucariatias@gmail.com>
2014-06-21 17:32:29 +0100

    Add a missing space

--- WaywardCharacters/src/main/java/net/wayward_realms/waywardcharacters/PlayerItemConsumeListener.java
+++ WaywardCharacters/src/main/java/net/wayward_realms/waywardcharacters/PlayerItemConsumeListener.java
@@ -30,7 +30,7 @@ public class PlayerItemConsumeListener implements Listener {
                         if (event.getItem().getItemMeta().hasLore()) {
                             if (event.getItem().getItemMeta().getLore().contains("+5 mana")) {
                                 character.setMana(Math.min(character.getMana() + 5, character.getMaxMana()));
-                                event.getPlayer().sendMessage(plugin.getPrefix() + ChatColor.GREEN + "+5 mana" + ChatColor.GRAY + " (" + character.getMana() + "/" + character.getMaxMana() + ")");
+                                event.getPlayer().sendMessage(plugin.getPrefix() + ChatColor.GREEN + "+5 mana " + ChatColor.GRAY + "(" + character.getMana() + "/" + character.getMaxMana() + ")");
                             }
                         }
                     }
@@ -40,12 +40,12 @@ public class PlayerItemConsumeListener implements Listener {
         if (event.getItem().getType() == Material.MILK_BUCKET) {
             Character character = plugin.getActiveCharacter(event.getPlayer());
             character.setThirst(character.getThirst() + 5);
-            event.getPlayer().sendMessage(ChatColor.GREEN + "Thirst: +5" + ChatColor.GRAY + " (Total: " + character.getThirst() + ")");
+            event.getPlayer().sendMessage(ChatColor.GREEN + "Thirst: +5 " + ChatColor.GRAY + "(Total: " + character.getThirst() + ")");
         }
         if (event.getItem().getType() == Material.ROTTEN_FLESH) {
             Character character = plugin.getActiveCharacter(event.getPlayer());
             character.setHealth(character.getHealth() - 5);
-            event.getPlayer().sendMessage(ChatColor.RED + "Health: -5" + ChatColor.GRAY + "(Total: " + character.getHealth() + ")");
+            event.getPlayer().sendMessage(ChatColor.RED + "Health: -5 " + ChatColor.GRAY + "(Total: " + character.getHealth() + ")");
         }
     }
 
