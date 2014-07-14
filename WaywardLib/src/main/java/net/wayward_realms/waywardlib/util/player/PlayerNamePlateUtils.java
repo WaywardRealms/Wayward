0fba15864da12337a5b58069e3077380bb30fd19
Lucariatias <lucariatias@gmail.com>
2014-07-14 15:59:08 +0100

    Revert "Remove all ProtocolLib stuff"
    This reverts commit 15431d20914224bab374c2091ad04ac366adf6de.
    
    Conflicts:
    	WaywardLib/pom.xml

--- WaywardLib/src/main/java/net/wayward_realms/waywardlib/util/player/PlayerNamePlateUtils.java
+++ WaywardLib/src/main/java/net/wayward_realms/waywardlib/util/player/PlayerNamePlateUtils.java
@@ -1,25 +1,26 @@
 package net.wayward_realms.waywardlib.util.player;
 
 import net.wayward_realms.waywardlib.Wayward;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class PlayerNamePlateUtils {
 
-    //private static PlayerDisplayModifier factory;
+    private static PlayerDisplayModifier factory;
 
     public static void init(Wayward plugin) {
-        //factory = new PlayerDisplayModifier(plugin);
+        factory = new PlayerDisplayModifier(plugin);
     }
 
     public static void refreshPlayer(Player player) {
-        //final PlayerNamePlateChangeEvent event = new PlayerNamePlateChangeEvent(player, player.getName(), player.getDisplayName());
-        //Bukkit.getPluginManager().callEvent(event);
-        //Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("WaywardLib"), new Runnable() {
-        //    @Override
-        //    public void run() {
-        //        factory.changeDisplay(event.getPlayer(), event.getSkin(), event.getName());
-        //    }
-        //});
+        final PlayerNamePlateChangeEvent event = new PlayerNamePlateChangeEvent(player, player.getName(), player.getDisplayName());
+        Bukkit.getPluginManager().callEvent(event);
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("WaywardLib"), new Runnable() {
+            @Override
+            public void run() {
+                factory.changeDisplay(event.getPlayer(), event.getSkin(), event.getName());
+            }
+        });
     }
 
 }
