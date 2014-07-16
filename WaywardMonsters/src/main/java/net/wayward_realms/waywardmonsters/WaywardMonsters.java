bd3e91505cb47e7882d97c0d00feeac75b0dbe4d
Lucariatias <lucariatias@gmail.com>
2014-07-16 23:45:29 +0100

    Add targets, closes #191

--- WaywardMonsters/src/main/java/net/wayward_realms/waywardmonsters/WaywardMonsters.java
+++ WaywardMonsters/src/main/java/net/wayward_realms/waywardmonsters/WaywardMonsters.java
@@ -8,6 +8,8 @@ import net.wayward_realms.waywardmonsters.bleed.BleedEntityDamageListener;
 import net.wayward_realms.waywardmonsters.bleed.BleedTask;
 import net.wayward_realms.waywardmonsters.drops.MobDrop;
 import net.wayward_realms.waywardmonsters.drops.MobDropManager;
+import net.wayward_realms.waywardmonsters.target.TargetProjectileHitListener;
+import net.wayward_realms.waywardmonsters.target.TargetSignChangeListener;
 import net.wayward_realms.waywardmonsters.trainingdummy.TrainingDummyPlayerInteractListener;
 import net.wayward_realms.waywardmonsters.trainingdummy.TrainingDummySignChangeListener;
 import org.bukkit.Location;
@@ -32,10 +34,13 @@ public class WaywardMonsters extends JavaPlugin implements MonstersPlugin {
         saveDefaultConfig();
         entityLevelManager = new EntityLevelManager(this);
         mobDropManager = new MobDropManager(this);
-        registerListeners(this,
+        registerListeners(
+                this,
+                new BleedEntityDamageListener(this), new BleedBlockBreakListener(),
                 new CreatureSpawnListener(this), new EntityDamageByEntityListener(this), new EntityDeathListener(this), new PlayerInteractEntityListener(), new PlayerFishListener(this),
-                new TrainingDummyPlayerInteractListener(this), new TrainingDummySignChangeListener(this),
-                new BleedEntityDamageListener(this), new BleedBlockBreakListener());
+                new TargetProjectileHitListener(this), new TargetSignChangeListener(this),
+                new TrainingDummyPlayerInteractListener(this), new TrainingDummySignChangeListener(this)
+                );
         getCommand("bleed").setExecutor(new BleedCommand(this));
         getCommand("zeropoint").setExecutor(new ZeroPointCommand(this));
         getCommand("viewzeropoints").setExecutor(new ViewZeroPointsCommand(this));
