473f9de2136f3f65d195b253cdcee4556039c3cc
Ross Binden <alyphen@gmail.com>
2016-04-01 21:45:31 +0100

    Prevent yaw ad pitch being swapped while unconscious

--- WaywardUnconsciousness/src/main/java/net/wayward_realms/waywardunconsciousness/PlayerMoveListener.java
+++ WaywardUnconsciousness/src/main/java/net/wayward_realms/waywardunconsciousness/PlayerMoveListener.java
@@ -16,7 +16,7 @@ public class PlayerMoveListener implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         if (plugin.isUnconscious(event.getPlayer())) {
             if (event.getFrom().getBlockX() != event.getTo().getBlockX() || event.getFrom().getBlockZ() != event.getTo().getBlockZ()) {
-                event.getPlayer().teleport(new Location(event.getFrom().getWorld(), event.getFrom().getBlockX() + 0.5, event.getFrom().getBlockY() + 0.5, (double) event.getFrom().getBlockZ(), event.getFrom().getPitch(), event.getFrom().getYaw()));
+                event.getPlayer().teleport(new Location(event.getFrom().getWorld(), event.getFrom().getBlockX() + 0.5, event.getFrom().getBlockY() + 0.5, (double) event.getFrom().getBlockZ(), event.getFrom().getYaw(), event.getFrom().getPitch()));
                 if (plugin.getServer().getPluginCommand("sleep") != null) plugin.getServer().dispatchCommand(event.getPlayer().getPlayer(), "sleep");
             }
         }
